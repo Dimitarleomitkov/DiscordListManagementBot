d74bec8051c1e6c8e190eabe659072986d4c1df6
undeadko <undeadko@abv.bg>
2023-05-13 11:25:11 +0300

    Added git cog for git commands through the bot

---
+++ cogs/git.py
@@ -0,0 +1,57 @@
+import discord
+from discord.ext import commands
+import git as Git
+import pathlib
+import os
+import cogs.lrlul_cogs as Cogs
+
+
+async def setup(bot):
+    await bot.add_cog(git(bot))
+    
+
+class git(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("Git module is loaded.")
+
+
+    @commands.command(  name = 'server_update',
+                        help = 'Pulls the code from the repo and reloads all cogs.',
+                        brief = 'Pulls the code from the repo and reloads all cogs.')
+    async def update_server(self, ctx):
+        text_chan = self.bot.get_channel(548554244932894750)
+
+        for filename in os.listdir(pathlib.Path(__file__).parent):
+            if filename.endswith(".py"):
+                # cut off the .py from the file name
+                cog = f"{filename[:-3]}"
+                # reload the cog
+                await self.bot.unload_extension(f"cogs.{cog}")
+                await self.bot.load_extension(f"cogs.{cog}")
+
+        await self.git_pull_func(ctx)
+
+
+
+    @commands.command(  name = 'git_pull',
+                        help = 'Performs git pull.',
+                        brief = 'Performs git pull.')
+    async def git_pull_func (self, ctx):
+        text_chan = self.bot.get_channel(548554244932894750)
+
+        try:
+            git_dir = pathlib.Path(__file__).parent.parent.resolve()
+            g = Git.cmd.Git(git_dir)
+            msg = g.pull()
+            await text_chan.send(str(msg))
+        except Exception as e:
+            await text_chan.send(e)
+
+
+
+
