98d446d12926e8afd1f1ae62253388060e29323a
undeadko <undeadko@abv.bg>
2023-05-13 04:12:22 -0200

    implemented the cooling_rpi functionality

--- cogs/cooling_rpi.py
+++ cogs/cooling_rpi.py
@@ -1,5 +1,9 @@
 import discord
 from discord.ext import commands
+import RPi.GPIO as GPIO
+
+
+GPIO.setwarnings(False)
 
 
 async def setup(bot):
@@ -20,6 +24,15 @@ class cooling_rpi(commands.Cog):
                         help = 'Turns the r-pi fan ON.',
                         brief = '- fan ON!')
     async def fan_on_func (self, ctx):
+        # Pin Definitons:
+        fan_GPIO = 18 # GPIO 18 PIN 12
+
+        # Pin Setup:
+        GPIO.setmode(GPIO.BCM) # BCM for naming BOARD for pin number
+        GPIO.setup(fan_GPIO, GPIO.OUT) # pin 12 set as output
+
+        # Pin control:
+        GPIO.output(18, GPIO.HIGH)
 
         await ctx.send("I am cool now <:aniguns:965289318585352212>")
 
@@ -28,6 +41,15 @@ class cooling_rpi(commands.Cog):
                         help = 'Turns the r-pi fan OFF.',
                         brief = '- fan OFF!')
     async def fan_off_func(self, ctx):
+        # Pin Definitons:
+        fan_GPIO = 18 # GPIO 18 PIN 12
+
+        # Pin Setup:
+        GPIO.setmode(GPIO.BCM) # BCM for naming BOARD for pin number
+        GPIO.setup(fan_GPIO, GPIO.OUT) # PWM pin set as output
+
+        # Pin control:
+        GPIO.output(18, GPIO.LOW)
 
         await ctx.send("<:loshoMiE:448905900279857153>")
 
