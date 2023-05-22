8ce137f3e3aa13ea751f665b27674f6409e950c5
Dimitar Mitkov <dimitar.mitkov@ewellix.com>
2023-05-22 10:06:06 +0300

    Initial implementation to start a new game with some debug info

--- cogs/ttt.py
+++ cogs/ttt.py
@@ -23,14 +23,16 @@ class ttt(commands.Cog):
     @commands.command(  name = 'ttt_new',
                         help = '>ttt_new @challenged',
                         brief = '- Start a new game of Tic-Tac-Toe with someone.')
-    async def ttt_new_game(self, ctx, challenged):
+    async def ttt_new_game(self, ctx, challenged: discord.User):
+        await ctx.message.delete()
+        
         try:
             self.player2 = ctx.author
             self.player1 = challenged
-            ctx.send(f"{self.player1}, {self.player2}")
+            await ctx.send(f"{self.player1}, {self.player2}")
             
-            ctx.send(f"{self.player2.mention} has been challenged to a Tic-Tac-Toe game by {self.player1.mention}")
-            ctx.send(f"https://tenor.com/view/yu-gi-oh-duel-yugi-anime-gif-7357665")
+            await ctx.send(f"{self.player2.mention} has been challenged to a Tic-Tac-Toe game by {self.player1.mention}")
+            await ctx.send(f"https://tenor.com/view/yu-gi-oh-duel-yugi-anime-gif-7357665")
             embed = discord.Embed(title = f"Tic-Tac-Toe {self.player1.mention} vs {self.player2.mention}")
             embed = discord.add_field(f"\
                                         | | | |        |1|2|3|\
@@ -41,6 +43,7 @@ class ttt(commands.Cog):
                                     ")
             embed.set_thumbnail(url = "https://e7.pngegg.com/pngimages/923/206/png-clipart-tictactoe-tic-tac-toe-oxo-tac-tic-toe-computer-icons-games-buttons-game-logo-thumbnail.png")
 
+            await ctx.send(embed = embed)
         except Exception as e:
             text_chan = self.bot.get_channel(548554244932894750)
             await text_chan.send(f"[Tic-Tac-Toe]\n{e}")
@@ -50,7 +53,7 @@ class ttt(commands.Cog):
 
     @commands.command( name = 'ttt',
                        help = '>ttt <field_number>',
-                       brief = 'Makes a play in the field.')
+                       brief = '- Makes a play in the field.')
     async def ttt_place(self, ctx, field):
         print(self.player1, self.player2)
         # if ctx.author != self.player1 || ctx.author != self.player2:
