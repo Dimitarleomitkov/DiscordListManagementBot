cbace4a31e5a8f4dea6abcfccf426910f7a134de
Dimitar Mitkov <dimitar.mitkov@ewellix.com>
2023-05-22 09:53:50 +0300

    debuging from another PC (useless commit)

--- cogs/ttt.py
+++ cogs/ttt.py
@@ -24,11 +24,11 @@ class ttt(commands.Cog):
                         help = '>ttt_new @challenged',
                         brief = '- Start a new game of Tic-Tac-Toe with someone.')
     async def ttt_new_game(self, ctx, challenged):
-        self.player2 = ctx.author
-        self.player1 = challenged
-        ctx.send(self.player1, self.player2)
-
         try:
+            self.player2 = ctx.author
+            self.player1 = challenged
+            ctx.send(f"{self.player1}, {self.player2}")
+            
             ctx.send(f"{self.player2.mention} has been challenged to a Tic-Tac-Toe game by {self.player1.mention}")
             ctx.send(f"https://tenor.com/view/yu-gi-oh-duel-yugi-anime-gif-7357665")
             embed = discord.Embed(title = f"Tic-Tac-Toe {self.player1.mention} vs {self.player2.mention}")
@@ -53,7 +53,6 @@ class ttt(commands.Cog):
                        brief = 'Makes a play in the field.')
     async def ttt_place(self, ctx, field):
         print(self.player1, self.player2)
-        ctx.send(self.player1, self.player2)
         # if ctx.author != self.player1 || ctx.author != self.player2:
         #     ctx.send(f"Other people are playing. Stop being rude!")
         #     ctx.send(f"https://tenor.com/view/really-dog-pup-puppy-head-shaking-gif-17699585")
