d3d378c847057e491f1780f3d0e1de0cabaa6ae6
undeadko <undeadko@abv.bg>
2023-06-07 21:03:16 +0300

    Spitting everything

--- cogs/spit.py
+++ cogs/spit.py
@@ -18,15 +18,19 @@ class spit(commands.Cog):
 
     # TODO: Make it spit on whatever I want. Not just @people
     @commands.command(name = 'spit')
-    async def spitting(self, ctx, *target_people: discord.User):
+    async def spitting(self, ctx, *target_people):
         await ctx.message.delete()
 
         if len(target_people) == 0:
             return
         
         for person in target_people:
-            if person is self.bot.user:
-                await ctx.send("https://tenor.com/view/llama-mirada-gif-8834856")
-            else:
-                await ctx.send(f"{person.mention}")
+            try:
+                if person is self.bot.user:
+                    await ctx.send("https://tenor.com/view/llama-mirada-gif-8834856")
+                else:
+                    await ctx.send(f"{person.mention}")
+                    await ctx.send(f"https://tenor.com/view/the-rock-spit-gif-20218743")
+            except Exception as e:
+                await ctx.send(f"@{person}")
                 await ctx.send(f"https://tenor.com/view/the-rock-spit-gif-20218743")
