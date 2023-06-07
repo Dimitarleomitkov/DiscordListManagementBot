80eace60d02c6b41885f329f4e7e29b403584765
undeadko <undeadko@abv.bg>
2023-06-07 21:21:24 +0300

    Finalizing spit.py

--- cogs/spit.py
+++ cogs/spit.py
@@ -16,21 +16,26 @@ class spit(commands.Cog):
         print("spit module is loaded.")
 
 
-    # TODO: Make it spit on whatever I want. Not just @people
     @commands.command(name = 'spit')
     async def spitting(self, ctx, *target_people):
         await ctx.message.delete()
 
         if len(target_people) == 0:
-            return
+            await ctx.send("https://tenor.com/view/llama-mirada-gif-8834856")
         
         for person in target_people:
             try:
-                if person is self.bot.user:
-                    await ctx.send("https://tenor.com/view/llama-mirada-gif-8834856")
+                if person.startswith("<@"):
+                    if person is self.bot.user:
+                        await ctx.send("https://tenor.com/view/llama-mirada-gif-8834856")
+                    else:
+                        await ctx.send(f"{person}")
+                        await ctx.send(f"https://tenor.com/view/the-rock-spit-gif-20218743")
                 else:
-                    await ctx.send(f"{person.mention}")
+                    if person.startswith('@'):
+                        await ctx.send(f"{person}")
+                    else:
+                        await ctx.send(f"@{person}")
                     await ctx.send(f"https://tenor.com/view/the-rock-spit-gif-20218743")
             except Exception as e:
-                await ctx.send(f"@{person}")
-                await ctx.send(f"https://tenor.com/view/the-rock-spit-gif-20218743")
+                print(e)
