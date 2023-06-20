f87f1dec0ab30e5f8503b3a2d77b65bb3a002a45
undeadko <undeadko@abv.bg>
2023-06-20 23:13:29 +0300

    Trying to fix server_restart command 4

--- cogs/git.py
+++ cogs/git.py
@@ -53,7 +53,7 @@ class git(commands.Cog):
             await text_chan.send("Booting a new instance of me...")
             
             try:
-                subprocess.run("source", "./boot.bash")
+                subprocess.run(["source", "./boot.bash"])
             except Exception as e:
                 await text_chan.send(e)
 
