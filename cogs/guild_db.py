5945cc616678a26c677bd403feaee86701e9ec21
undeadko <undeadko@abv.bg>
2024-04-13 18:46:11 +0300

    Fixed datetime

--- cogs/guild_db.py
+++ cogs/guild_db.py
@@ -1,5 +1,5 @@
 import discord
-from datetime import datetime
+import datetime
 from discord.ext import commands
 import sqlalchemy as db
 from sqlalchemy import (desc, create_engine, MetaData, Table, Column, Integer, String, DateTime, Boolean, select, text, delete, update)
